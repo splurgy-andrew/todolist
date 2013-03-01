cd64d62a4d022f6ac43144e591708c9e86bcb8a8
Andrew Lee <andrew@splurgy.org>
2013-02-28 16:12:50 -0800

    working steps.rb

--- features/step_definitions/tasks_steps.rb
+++ features/step_definitions/tasks_steps.rb
@@ -1,31 +1,43 @@
 Given /^I am on the todo list page/i do
-  pending
+  visit '/tasks/'
 end
 
 Then /^all of the tasks show/i do
-  pending
+  if Task.all.length == 0
+    page.should have_content('there is nothing here, add a task')
+  else
+    page.should have_content('#<Task id')
+  end
 end
 
 When /^I add a task/i do
-  pending
+  Task.create!(:title => "Finish cucumber tests", :position => 1, :completed => false)
 end
 
 Then /^the added task should show/i do
-  pending
+  visit '/tasks/'
+  task = Task.find_by_position(1)
+  page.should have_content (task.title)
 end
 
 When /^I delete a task/i do
-  pending
+  task = Task.create!(:title => "Finish cucumber tests", :position => 1, :completed => false)
 end
 
 Then /^the deleted task should not show/i do
-  pending
+  task = Task.find_by_position(1)
+  task.delete
+  visit '/tasks/'
+  page.should_not have_content(task.title)
 end
 
 When /^I mark a task as complete/i do
-  pending
+  task = Task.create!(:title => "Finish cucumber tests", :position => 1, :completed => false)
+  task.update_attribute(:completed, true)
 end
 
 Then /^the task should be marked as complete/i do
-  pending
+  task = Task.find_by_position(1)
+  visit '/tasks/'
+  page.should have_content (true)
 end
